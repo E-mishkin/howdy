4db9f461d79bdc70f5847e5f0cac8881fc559aa6
Evgenii Mishkin <emishkin@mail.greenriver.edu>
2020-01-10 13:46:53 -0500

    Add views folder

--- index.php
+++ index.php
@@ -10,10 +10,17 @@ require_once('vendor/autoload.php');
 //Create an instance of the base class
 $f3 = Base::instance();
 
-
+/*
 //Define a default route
 $f3->route('GET /', function (){
     echo '<h1>Howdy!</h1>';
+});
+*/
+
+//Define a default route
+$f3->route('GET /', function (){
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run fat free
